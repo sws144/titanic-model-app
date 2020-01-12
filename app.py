865d271dec36e672a225fe8beb9777bac9d5ade2
Stanley Wang <s.wang4331@gmail.com>
2020-01-11 22:18:08 -0500

    update main with model

--- app.py
+++ app.py
@@ -11,27 +11,48 @@ model = pickle.load(open('model.pkl','rb'))
 app = Flask(__name__, template_folder='templates')
 
 # routes
-@app.route('/',methods=['POST'])
-def predict():
-    # get data
-    data = request.get_json(force = True)
-    
-    # convert to dataframe
-    data.update((x,[y]) for x , y in data.items())  
-    data_df = pd.DataFrame.from_dict(data)
-    
-    # predictions
-    result = model.predict(data_df)
-    
-    # send back to browser
-    output  = {'results': int(result[0])}
-    
-    # return data
-    return jsonify(result=output)
-
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def main():
-    return(render_template('main.html'))
+    if request.method == 'GET':
+        return(render_template('main.html'))
+    if request.method == 'POST':
+        pclass = request.form['Pclass']
+        age = request.form['Age']
+        sibSp = request.form['SibSp']
+        fare = request.form['Fare']        
+        
+        input_variables = pd.DataFrame([[pclass, age, sibSp, fare]],
+                columns=['Pclass', 'Age', 'SibSp','Fare'],
+                dtype=int)        
+        
+        prediction = model.predict(input_variables)[0]        
+        
+        return render_template('main.html',
+                original_input={'Pclass': pclass,
+                                'Age':age,
+                                'Sibsp':sibSp,
+                                'Fare': fare},
+                result=prediction
+        )
+
+# def predict():
+#     # get data
+#     data = request.get_json(force = True)
+    
+#     # convert to dataframe
+#     data.update((x,[y]) for x , y in data.items())  
+#     data_df = pd.DataFrame.from_dict(data)
+    
+#     # predictions
+#     result = model.predict(data_df)
+    
+#     # send back to browser
+#     output  = {'results': int(result[0])}
+    
+#     # return data
+#     return jsonify(result=output)
+
+
 
 if __name__ == '__main__':
     app.run()
